e7060689458213ad6f6d1f54079ba46074f8a754
grsakea <grsakea@users.noreply.github.com>
2017-12-11 23:33:21 +0100

    Add test NewSession

--- common_test.go
+++ common_test.go
@@ -1,8 +1,21 @@
 package twitch
 
 import (
+	"os"
 	"testing"
 )
+
+func TestNewSession(t *testing.T) {
+	old_env := os.Getenv("CLIENT_ID")
+	os.Setenv("CLIENT_ID", "foobar")
+	defer os.Setenv("CLIENT_ID", old_env)
+
+	s := NewSession()
+	if s.ClientID != "foobar" {
+		t.Fatal("Failed creating session")
+	}
+
+}
 
 func TestImplementInterface(t *testing.T) {
 	var twitch TwitchInterface = (*Session)(nil)
